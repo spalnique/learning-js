92da6d16a28ad20611c7b8e7806f5136ef05bead
Oleksii_S <lex.sydoruk@gmail.com>
2024-01-04 21:44:02 +0100

    almost done

--- js/random-movie.js
+++ js/random-movie.js
@@ -1,40 +1,68 @@
+////////// Тут імпортуються та перетворюються дані, описуються функції, 
+
 import { jsonData } from './data.js';
 
 const moviesArray = JSON.parse(jsonData);
 
-async function createMovieBlock() {
-  const randomIndex = Math.round(moviesArray.length * Math.random());
-  const movieBlock = document.createElement('div');
-  movieBlock.classList.add('movie-container');
-  const poster = document.createElement('img');
-  poster.classList.add('movie-image');
-  poster.width = 300;
-  poster.height = 450;
-  poster.src = `${moviesArray[randomIndex]['Poster']}`;
-  poster.alt = `${moviesArray[randomIndex]['Title']}`;
-  movieBlock.append(poster);
-  container.append(movieBlock);
+function makeTag(tagName, classes, attObj, innerText) {
+  const html = document.createElement(tagName);
+  if (Array.isArray(classes)) {
+    classes.forEach(x => html.classList.add(x));
+  } else {
+    html.classList.add(classes);
+  }
+  for (const attribute in attObj) {
+    html.setAttribute(`${attribute}`, `${attObj[attribute]}`);
+  }
+  html.textContent = innerText;
+
+  return html;
 }
-const header = document.createElement('header');
-header.classList.add('header-container');
-const logo = document.createElement('a');
-logo.classList.add('logo');
-logo.setAttribute('href', 'index.html');
-logo.textContent = '__ RandoMovie.JS';
-header.append(logo);
-const main = document.createElement('main');
-const container = document.createElement('div');
-container.classList.add('container');
-main.append(container);
 
-const footer = document.createElement('footer');
-footer.classList.add('footer-container');
-
-document.body.prepend(header, main, footer);
-
-for (let i = 0; i < 3; i++) {
-  createMovieBlock();
+function randomMoviePicker(quantity) {
+  for (let i = 0; i < quantity; i++) {
+    const randomIndex = Math.round(moviesArray.length * Math.random());
+    const content = makeTag('div', 'movie-container');
+    const poster = makeTag('img', 'movie-image', {
+      width: 300,
+      height: 450,
+      src: moviesArray[randomIndex]['Poster'],
+      alt: moviesArray[randomIndex]['Title'],
+    });
+    content.append(poster);
+    container.insertAdjacentHTML('afterbegin', content.outerHTML);
+  }
 }
+
+function assembleBaseHtml(obj) {
+  for (const item in obj) {
+    const parent = document.querySelector(item);
+    for (const child of obj[item]) {
+      parent.append(child);
+    }
+  }
+}
+
+////////// Після цього вже руцями викликаємо створюємо бажані елементи та викликаємо відповідні функції //////////
+
+const header = makeTag('header', ['header', 'header-container']);
+const main = makeTag('main');
+const footer = makeTag('footer', ['footer', 'footer-container']);
+const container = makeTag('div', 'container');
+const logoLink = makeTag(
+  'a',
+  ['logo-link', 'test'],
+  { href: 'index.html', name: 'sitelogo', alt: 'random' },
+  '__ RandoMovie.JS',
+);
+
+assembleBaseHtml({
+  body: [header, main, footer],
+  header: [logoLink],
+  main: [container],
+});
+
+randomMoviePicker(3);
 
 const button = document.createElement('button');
 button.classList.add('regenerate');
@@ -44,39 +72,3 @@ main.append(button);
 button.addEventListener('click', () => {
   location.reload();
 });
-
-// async function fetchData() {
-//   let incomingData;
-
-//   try {
-//     const response = await fetch(
-//       'https://raw.githubusercontent.com/toedter/movies-demo/master/backend/src/main/resources/static/movie-data/movies-250.json',
-//     );
-//     const data = await response.json();
-
-//     incomingData = data.movies;
-//     console.log(incomingData);
-
-//     // Викликати функцію або виконати інші дії, що залежать від отриманих даних
-//     doSomethingWithIncomingData();
-//   } catch (error) {
-//     console.error('Помилка завантаження файлу:', error);
-//   }
-// }
-
-// function doSomethingWithIncomingData() {
-//   // Тут ви можете використовувати `incomingData`
-//   console.log('Дії, що залежать від отриманих даних', incomingData);
-// }
-
-// fetchData();
-
-// const movieBlock = document.createElement('div');
-// movieBlock.classList.add('movie-container');
-// movieBlock.innerHTML = `<img
-//                         class="movie-image"
-//                         width="300"
-//                         height="450"
-//                         src="${moviesArray[randomIndex]['Poster']}"
-//                         alt="${moviesArray[randomIndex]['Title']}">`;
-// document.body.insertAdjacentElement('afterbegin', movieBlock);
